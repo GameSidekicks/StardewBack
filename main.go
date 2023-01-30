6b3645870481779ec0bd3dbd0a59320066eb2340
agu2tin <agu2tin.g@gmail.com>
2023-01-29 21:26:41 -0300

    First version of the scraper with JSON output

--- main.go
+++ main.go
@@ -1,22 +1,77 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
 
 	"github.com/gocolly/colly"
 )
 
+type Villa struct {
+	Name     string `json:"name"`
+	Birthday string `json:"birthday"`
+}
+
 func main() {
-	c := colly.NewCollector(
-		colly.AllowedDomains("en.wikipedia.org"),
+
+	allVillagers := make([]Villa, 0)
+
+	// Init collectors
+	cacheDir := filepath.Join("cache")
+
+	collVillager := colly.NewCollector(
+		colly.CacheDir(cacheDir),
 		colly.AllowedDomains("stardewvalleywiki.com"),
 	)
 
-	// Find and print all links
-	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
-		links := e.ChildAttrs("a", "href")
-		fmt.Println(links)
+	// Feedback: which URL are we scraping
+	collVillager.OnRequest(func(r *colly.Request) {
+		fmt.Println("Scraping:", r.URL)
 	})
-	c.Visit("https://stardewvalleywiki.com/Cooking")
 
+	// Feedback: response status
+	collVillager.OnResponse(func(r *colly.Response) {
+		fmt.Println("Status:", r.StatusCode)
+	})
+
+	// Error
+	collVillager.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	collVillager.OnHTML(".mw-parser-output tr", func(t *colly.HTMLElement) {
+		villager := t.ChildText("td:nth-child(1)")
+		birth := t.ChildText("td:nth-child(2)")
+
+		if villager != "" && villager != "Universals" {
+
+			dataTable := Villa{
+				Name:     villager,
+				Birthday: birth,
+			}
+			allVillagers = append(allVillagers, dataTable)
+		}
+
+	})
+
+	collVillager.Visit("https://stardewvalleywiki.com/List_of_All_Gifts")
+
+	// enc := json.NewEncoder(os.Stdout)
+	// enc.SetIndent("", " ")
+	// enc.Encode(allVillagers)
+
+	writeJson(allVillagers)
+}
+
+func writeJson(data []Villa) {
+	file, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		log.Println("Unable to create json file")
+		return
+	}
+
+	_ = ioutil.WriteFile("villagers.json", file, 0644)
 }
